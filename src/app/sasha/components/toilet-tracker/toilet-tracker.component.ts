a3b3e20be0dcd19555ed56b2d8ff0cec27f30281
Sam Keel <samkeel@protonmail.com>
2023-06-11 11:08:06 +0800

    star rating

--- src/app/sasha/components/toilet-tracker/toilet-tracker.component.ts
+++ src/app/sasha/components/toilet-tracker/toilet-tracker.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Observable, filter } from 'rxjs';
 import { BpObserverService } from 'src/app/shared/services/bp-observer.service';
 
 @Component({
   selector: 'app-toilet-tracker',
   templateUrl: './toilet-tracker.component.html',
-  styleUrls: ['./toilet-tracker.component.scss']
+  styleUrls: ['./toilet-tracker.component.scss'],
 })
-export class ToiletTrackerComponent {
+export class ToiletTrackerComponent implements OnInit {
+  @Input() maxRating = 5;
+  @Input() selectedStar = 0;
+  maxRatingArr: any = [];
+  previousSelection = 0;
+
   isHandsetPortrait$: Observable<boolean> = this.bpoService.HandsetPortrait$;
 
-  constructor(
-    private bpoService: BpObserverService,
-  ) {}
+  form = this.fb.group({
+    bmDate: [new Date(), Validators.required],
+  });
 
+  constructor(private bpoService: BpObserverService, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.maxRatingArr = Array(this.maxRating).fill(0);
+  }
+
+  HandleMouseEnter(index: number) {
+    this.selectedStar = index + 1;
+  }
+
+  HandleMouseLeave() {
+    if (this.previousSelection !== 0) {
+      this.selectedStar = this.previousSelection;
+    } else {
+      this.selectedStar = 0;
+    }
+  }
+
+  Rating(index: number) {
+    this.selectedStar = index + 1;
+    this.previousSelection = this.selectedStar;
+  }
 }
--- src/app/sasha/sasha.module.ts
+++ src/app/sasha/sasha.module.ts
@@ -5,19 +5,32 @@ import { SashaRoutingModule } from './sasha-routing.module';
 import { SashaHomeComponent } from './components/main/sasha-home/sasha-home.component';
 import { SashaHeaderComponent } from './components/main/sasha-header/sasha-header.component';
 import { OverviewComponent } from './components/overview/overview.component';
-// 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MatTabsModule } from '@angular/material/tabs';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
 import { ToiletTrackerComponent } from './components/toilet-tracker/toilet-tracker.component';
 import { NutritionistComponent } from './components/nutritionist/nutritionist.component';
 import { DrAppointmentsComponent } from './components/dr-appointments/dr-appointments.component';
-
+//
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @NgModule({
-  declarations: [SashaHomeComponent, SashaHeaderComponent, OverviewComponent, ToiletTrackerComponent, NutritionistComponent, DrAppointmentsComponent],
+  declarations: [
+    SashaHomeComponent,
+    SashaHeaderComponent,
+    OverviewComponent,
+    ToiletTrackerComponent,
+    NutritionistComponent,
+    DrAppointmentsComponent,
+  ],
   imports: [
     CommonModule,
     SashaRoutingModule,
@@ -26,6 +39,13 @@ import { DrAppointmentsComponent } from './components/dr-appointments/dr-appoint
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
 })
 export class SashaModule {}
