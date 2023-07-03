0246339be8bb5e6ce15efd3d477554f10089d660
Sam Keel <samkeel@protonmail.com>
2023-07-03 20:23:43 +0800

    tab events

--- src/app/sasha/components/main/sasha-home/sasha-home.component.ts
+++ src/app/sasha/components/main/sasha-home/sasha-home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-sasha-home',
   templateUrl: './sasha-home.component.html',
-  styleUrls: ['./sasha-home.component.scss']
+  styleUrls: ['./sasha-home.component.scss'],
 })
 export class SashaHomeComponent {
+  activeTab: number = 1;
 
+  onTabChange(event: MatTabChangeEvent) {
+    this.activeTab = event.index + 1;
+  }
 }
